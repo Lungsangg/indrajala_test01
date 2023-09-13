96c470186ae024cef38493da219a8348085bca5e
lungsang-bod <lungsang@esukhia.org>
2023-09-13 18:03:26 +0530

    settings_update

--- sefaria/settings.py
+++ sefaria/settings.py
@@ -316,10 +316,9 @@ CACHES = {
 
 GLOBAL_INTERRUPTING_MESSAGE = None
 
-REMOTE_HOSTS = os.getenv('REMOTE_HOSTS', 'indrajala.com', 'indrajala-test.onrender.com').replace(" ", "")
+REMOTE_HOSTS = os.getenv('REMOTE_HOSTS', 'indrajala.com').replace(" ", "")
 
 LOCAL_HOSTS = [
-    'indrajala-test.onrender.com'
     'indrajala.com',
     'localhost',
     '127.0.0.1',
